f227d73a69d2875a710f9d9fa337179c773efe39
raspy718 <rasp.bklyn@gmail.com>
2026-01-18 15:44:04 -0500

    chat bubbles

--- src/App.tsx
+++ src/App.tsx
@@ -1155,16 +1155,16 @@ const handleAddReaction = (msgId, emoji) => {
                                         </div>
                                     )}
                                     
-                                    {/* TAIL SVG - Sent (Right) - Rotated Clockwise 6deg */}
+                                    {/* TAIL SVG - Sent (Right) - Seamless Overlay (z-20) */}
                                     {isMe && isLastInGroup && !msg.imageUrl && (
-                                        <svg className="absolute bottom-[1px] -right-[9px] rotate-[6deg] z-0 w-[17px] h-[14px] fill-current" viewBox="0 0 17 14">
+                                        <svg className="absolute bottom-[1px] -right-[8px] rotate-[6deg] z-20 w-[17px] h-[14px] fill-current" viewBox="0 0 17 14">
                                             <path d="M0,0 C2,7.5 9,14 17,14 H0 V0 Z" fill={tailColor} />
                                         </svg>
                                     )}
 
-                                    {/* TAIL SVG - Received (Left) - Rotated Counter-Clockwise 6deg (Symmetry) */}
+                                    {/* TAIL SVG - Received (Left) - Seamless Overlay (z-20) */}
                                     {!isMe && isLastInGroup && !msg.imageUrl && (
-                                        <svg className="absolute bottom-[1px] -left-[9px] rotate-[-6deg] z-0 w-[17px] h-[14px] fill-current" viewBox="0 0 17 14">
+                                        <svg className="absolute bottom-[1px] -left-[8px] rotate-[-6deg] z-20 w-[17px] h-[14px] fill-current" viewBox="0 0 17 14">
                                             <path d="M17,0 C15,7.5 8,14 0,14 H17 V0 Z" fill={tailColor} />
                                         </svg>
                                     )}
